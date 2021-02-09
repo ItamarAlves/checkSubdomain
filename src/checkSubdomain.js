7916d5f36f4b61c13dc7cc24fa0e7fbf8509c33f
ItamarAlves <itamaralvesprofi@gmail.com>
2021-02-08 21:20:45 -0400

    adding to pass the file path

--- src/checkSubdomain.js
+++ src/checkSubdomain.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const request = require('request')
 
 function checkSubdomain() {
-    fs.readFile('arquivo/url.txt','utf8', function(err, data){
+    const [dir] = process.argv.slice(2)
+    if (dir == undefined) {
+        console.error("Favor informar o caminho do arquivo que está suas URL, exemplo: node src/checkSubdomain.js arquivo/url.txt")
+        process.exit()
+    }
+
+    fs.readFile(dir,'utf8', function(err, data){
         var lines = data.split(/\r?\n/);
         
         lines.forEach(function(line) {
