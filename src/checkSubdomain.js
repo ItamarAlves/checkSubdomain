1e89d23fb9593b77b184e70d238364bd3d9bbfa7
ItamarAlves <itamaralvesprofi@gmail.com>
2021-02-08 20:16:48 -0400

    add checkSubmain function

---
+++ src/checkSubdomain.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const request = require('request')
+
+function checkSubdomain() {
+    fs.readFile('arquivo/url.txt','utf8', function(err, data){
+        var lines = [];
+        lines = data.split(/\r?\n/);
+        
+        lines.forEach(function(line) {
+            request('https://'+line, function(err, response) {
+                if (response && response.statusCode == 200) {
+                    console.log(line + ' - ' + response.statusCode)
+                }
+            })
+        })
+    });
+}
+
+checkSubdomain()
